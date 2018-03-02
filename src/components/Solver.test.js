602aaa82e4680e3a74a5f417d1b6d4ca64b20dbf
bbr <bj@webedia.fr>
2018-03-02 17:26:27 +0100

    near to finish // TODO last part here :ReactReduxTestingUsingJestEnzyme // Last home test

--- src/components/Solver.test.js
+++ src/components/Solver.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import SolverConnected, { Solver } from './Solver';
+import { shallow, mount } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
+
+import SolverConnected, { Solver } from './Solver';
+import { solveIt, loadGrid, clearIt } from '../actions';
+import * as Actions from '../actions/Constants';
 
 describe('>>> Solver --- render components', () => {
     let wrapper;
@@ -48,14 +51,36 @@ describe('>>> Solver - REDUX', () => {
     it('+++ check initial state', () => {
         expect(wrapper.prop('haveError')).toBe(!state.isOk);
     });
+});
 
-    // it('+++ render solve should be enable', () => {
-        // expect(wrapper.find('button:disabled')).toHaveLength(0);
-    // });
+describe('>>> Solver - React + REDUX (mount + wrapping provider) ', () => {
+    let wrapper, store;
+    let mockStore = configureStore()
+    let state = { isOk: true };
 
-    // it('+++ render solve is disabled', () => {
-        // expect(wrapper.find('button:disabled')).toHaveLength(1);
-    // });
+    beforeEach(()=>{
+        store = mockStore(state);
+        wrapper = mount( <Provider store={store}><SolverConnected /></Provider> );
+    });
 
+    it('+++ contains all actions', () => {
+        expect(wrapper.find('button')).toHaveLength(4);
+    });
+
+    it('+++ render the connected(SMART) component', () => {
+       expect(wrapper.find(SolverConnected).length).toEqual(1)
+    });
+
+    it('+++ check action on dispatching ', () => {
+        store.dispatch(solveIt());
+        store.dispatch(clearIt());
+        store.dispatch(loadGrid(0));
+
+        let actions = store.getActions()
+
+        expect(actions[0].type).toBe(Actions.SOLVE);
+        expect(actions[1].type).toBe(Actions.CLEAR);
+        expect(actions[2].type).toBe(Actions.LOAD_GRID);
+    });
 
 });
